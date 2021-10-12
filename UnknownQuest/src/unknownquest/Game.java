10d8ff90c95ba9ce1ca89b1641fe49197c06cef3
Giovanny Quimbayo <giovannyqtech@gmail.com>
2021-10-12 17:04:08 -0400

    added intro file and a introduction game info

--- UnknownQuest/src/unknownquest/Game.java
+++ UnknownQuest/src/unknownquest/Game.java
@@ -1,19 +1,23 @@
 package unknownquest;
 
-import java.util.Scanner;
+import java.io.File;
 
 public class Game {
 
-	public Game() {	
+	public Game(boolean runGame) {	
 		
 		
+		if(runGame) {
 		Level firstStage = new Level("The house", "entrance","it looks like a door", "living room", "there are a lot of furnitures", "hallway", 
 				"it is dark and long", "bed room", "it is big and abed is there");		
 		Node firstRooms = new Node(firstStage);
 		DoublyLinked stages = new DoublyLinked();
 		stages.setHead(firstRooms);
 		
-		System.out.println(stages.getHead().getLevel().getLevelName());
+		System.out.println("You are in the game");
+		}
+		
+		
 		
 	}	
 	
--- UnknownQuest/src/unknownquest/UnknownQuest.java
+++ UnknownQuest/src/unknownquest/UnknownQuest.java
@@ -1,13 +1,48 @@
 
 package unknownquest;
-
+import java.io.File;
+import java.util.Scanner;
 
 public class UnknownQuest {
-
+	
+	static boolean startGame = false; 
    
     public static void main(String[] args) {
-        Game game = new Game();
+
+    	//reading the introduction file
+    	try {
+    		File introFile = new File("src/intro.txt");
+    		Scanner intro = new Scanner(introFile);
+    		while(intro.hasNextLine()) {
+    			System.out.println(intro.nextLine());
+    		}
+    		intro.close();    		
+		} catch(Exception e) {
+			System.out.println("wrong file path!!");
+		}	    	
+    	playerInfo();
+    	Game game = new Game(startGame);
         
     }
+    
+    public static void playerInfo() {
+    	//player input name
+    	Scanner in = new Scanner(System.in);
+    	System.out.println("what is your name hero: ");
+    	String heroName = in.nextLine();   	    	
+    	
+    	System.out.println("Hello " + heroName + " Would you like to start the journey.");    	
+    	String start = in.nextLine();
+    	
+    	if(start.equals("yes")) {
+    		startGame = true;
+    		PlayerNpc gameHero = new PlayerNpc(heroName, 60, 0);
+    		in.close();
+    	}
+    	else {    		
+    		System.out.println("Good choice you would have fail anyways, good luck");    		
+    	}	
+    }
+    
     
 }
